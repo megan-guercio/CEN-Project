85a04eb35c349707af3da3ae5f9793c22e2c7731
Sarah Osborn <sao16b@my.fsu.edu>
2019-09-07 17:19:31 -0400

    Server side isloggedin verification

--- CEN-Project/CEN-Project/Site.Master.cs
+++ CEN-Project/CEN-Project/Site.Master.cs
@@ -10,19 +10,26 @@ namespace CEN_Project
 {
     public partial class SiteMaster : MasterPage
     {
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            //better way of doing this?
+            if (Request.Url.LocalPath == "/default.aspx") return;
+            else if (Page.Request.Form.ToString().Contains("btnLogin=Login")) return;
+            else if (Page.Request.Form.ToString().Contains("btnRegister=Register")) return;
+            else if (Convert.ToBoolean(Session["loggedIn"])) return;
 
+            Page.ClientScript.RegisterStartupScript(GetType(), "LoggedIn", "<script type='text/javascript'>login()</script>");
         }
 
         protected void CreateNewUser(object sender, EventArgs e)
         {
-            Page.ClientScript.RegisterStartupScript(GetType(), "LoggedIn", "<script type='text/javascript'>sessionStorage.setItem('status', 'loggedin');</script>");
+            Session["loggedIn"] = true;
         }
 
         protected void LogIn(object sender, EventArgs e)
         {
-            Page.ClientScript.RegisterStartupScript(GetType(), "LoggedIn", "<script type='text/javascript'>sessionStorage.setItem('status', 'loggedin');</script>");
+            Session["loggedIn"] = true;
         }
     }
 }
