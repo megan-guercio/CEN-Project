caf8adbce48235c30559974324354fe10120a5d6
Sarah Osborn <sao16b@my.fsu.edu>
2019-10-08 13:24:50 -0400

    Registration and Login are working
    The user is now allowed to login/register for the site using Firebase libraries.

--- CEN-Project/CEN-Project/Site.Master.cs
+++ CEN-Project/CEN-Project/Site.Master.cs
@@ -5,6 +5,12 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Web.Security;
+using Firebase;
+using Firebase.Auth;
+using FirebaseAdmin;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis;
+using Firebase.Database;
 
 namespace CEN_Project
 {
@@ -14,29 +20,36 @@ namespace CEN_Project
         protected void Page_Load(object sender, EventArgs e)
         {
             //better way of doing this?
-            if (Convert.ToBoolean(Session["loggedIn"]))
+            if (Session["curUser"] != null)
             {
                 lbProfile.Style["display"] = "inline-block";
                 return;
             }
-            else if (Request.Url.LocalPath == "/default.aspx" || Request.Url.LocalPath == "/About") return;
+            else if (Request.Url.LocalPath.ToLower().Contains("default") || Request.Url.LocalPath.ToLower().Contains("about")) return;
             else if (Page.Request.Form.ToString().Contains("btnLogin=Login")) return;
             else if (Page.Request.Form.ToString().Contains("btnRegister=Register")) return;
-            
-
-            Page.ClientScript.RegisterStartupScript(GetType(), "LoggedIn", "<script type='text/javascript'>login()</script>");
+            if (Session["curUser"] == null) Page.ClientScript.RegisterStartupScript(GetType(), "LoggedIn", "<script type='text/javascript'>loginPopUp()</script>");
         }
-
         protected void CreateNewUser(object sender, EventArgs e)
         {
-            Session["loggedIn"] = true;
             lbProfile.Style["display"] = "inline-block";
         }
 
         protected void LogIn(object sender, EventArgs e)
         {
-            Session["loggedIn"] = true;
             lbProfile.Style["display"] = "inline-block";
+            System.Threading.Tasks.Task.Run(async () => await VerifyLogIn());
+        }
+
+        protected async System.Threading.Tasks.Task VerifyLogIn()
+        {
+            lbProfile.Style["display"] = "inline-block";
+            var defaultApp = FirebaseApp.Create(new AppOptions()
+            {
+                Credential = GoogleCredential.FromFile(Server.MapPath("~") + @"\Scripts\cen-project-d757f-firebase-adminsdk-k6z2f-3a1bdfe3c4.json")
+            });
+            var auth = FirebaseAdmin.Auth.FirebaseAuth.GetAuth(defaultApp);
+            Session["curUser"] = await auth.GetUserByEmailAsync(UserName.Text);
         }
     }
 }
