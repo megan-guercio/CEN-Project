09884efc9e586b1f398b8317b6718673b1afd882
Sarah Osborn <sao16b@my.fsu.edu>
2019-10-09 11:54:36 -0400

    Added FSU email constraint, access to user database
    Only people with fsu.edu in their email addresses will be allowed to register. Access to the user database has been established, and data is added with every new registration.

--- CEN-Project/CEN-Project/Site.Master.cs
+++ CEN-Project/CEN-Project/Site.Master.cs
@@ -11,12 +11,13 @@ using FirebaseAdmin;
 using Google.Apis.Auth.OAuth2;
 using Google.Apis;
 using Firebase.Database;
+using Google.Cloud.Firestore;
+using System.Diagnostics;
 
 namespace CEN_Project
 {
     public partial class SiteMaster : MasterPage
     {
-
         protected void Page_Load(object sender, EventArgs e)
         {
             //better way of doing this?
@@ -38,16 +39,36 @@ namespace CEN_Project
         protected void LogIn(object sender, EventArgs e)
         {
             lbProfile.Style["display"] = "inline-block";
-            lbProfile.Style["display"] = "inline-block";
-            
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", Server.MapPath("~") + @"Scripts\cen-project-d757f-firebase-adminsdk-k6z2f-53b4c90b47.json");
+
             var defaultApp = FirebaseApp.Create(new AppOptions()
             {
-                Credential = GoogleCredential.FromFile(Server.MapPath("~") + @"Scripts\cen-project-d757f-firebase-adminsdk-k6z2f-53b4c90b47.json")
+                Credential = GoogleCredential.GetApplicationDefault()
             });
+
             var auth = FirebaseAdmin.Auth.FirebaseAuth.GetAuth(defaultApp);
             Session["curUser"] = auth.GetUserByEmailAsync(UserName.Text).Result;
             Page.ClientScript.RegisterStartupScript(GetType(), "LoggedIn", "<script type='text/javascript'>console.log('" + Session["curUser"] + "');</script>");
-        }
 
+            if (Session["curUser"] == null) return;
+
+            //Check if user is in database
+            var x = (FirebaseAdmin.Auth.UserRecord)Session["curUser"];
+            FirestoreDb db = FirestoreDb.Create("cen-project-d757f");
+
+            CollectionReference usersRef = db.Collection("users");
+            
+            DocumentReference docref = db.Collection("users").Document(x.Uid);
+            DocumentSnapshot snapshot = docref.GetSnapshotAsync().Result;
+            //User already exists in database. Login complete
+            if (snapshot.Exists) return;
+            //Add user to database
+            Dictionary<string, string> user = new Dictionary<string, string>
+            {
+                {"userName", x.Email.Substring(0, x.Email.IndexOf('@')) }
+            };
+            var d = docref.SetAsync(user).Result;
+
+        }
     }
 }
--- CEN-Project/CEN-Project/Site.Master.designer.cs
+++ CEN-Project/CEN-Project/Site.Master.designer.cs
@@ -137,5 +137,14 @@ namespace CEN_Project {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnRegister;
+        
+        /// <summary>
+        /// errorNonFSU control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl errorNonFSU;
     }
 }
