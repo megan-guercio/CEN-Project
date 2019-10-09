b6ad344d9e41dffcbad48601c4c4c725337491f8
sao16b <35247589+sao16b@users.noreply.github.com>
2019-10-09 17:35:06 -0400

    Delete InstallApp.ps1

--- CEN-Project/InstallApp.ps1
+++
@@ -1,7 +0,0 @@
-"C:Program Files\dotnet\dotnet.exe" restore
-"C:Program Files\dotnet\dotnet.exe" publish --configuration release -o c:\CENProject\publish --runtime active
-C:\Windows\SysNative\WindowsPowerShell\v1.0\powershell.exe -Command {
-             Import-Module WebAdministration
-             Set-ItemProperty 'IIS:sitesDefault Web Site' 
-                 -Name physicalPath -Value c:\CENProject\publish
-}
